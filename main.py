b49e09f3188d6df4117280fa4f3cb44f3a4bb79f
NV-7 <naviboun@gmail.com>
2026-08-20 21:49:35 -0500

    Chunking and embedding one documment
    Selected one document to test embedding and chunking. Extracted text using docling

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pandas as pd
 from datasets import load_dataset
 from dotenv import load_dotenv
 from sklearn.model_selection import train_test_split
+from docling.document_converter import DocumentConverter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
 
 load_dotenv()
 
@@ -28,6 +31,8 @@ print(unique_doc['doc_link'])
 
 # Download the documments from the link in the dataframe and save them as pdfs.
 os.makedirs("pdfs", exist_ok=True)
+
+failed_docs = []
 for _, row in unique_doc.iterrows():
     name = row['doc_name']
     url = row['doc_link']
@@ -39,10 +44,28 @@ for _, row in unique_doc.iterrows():
             print(f"{name} downloaded successfully.")
         else:
             print(f"Failed to download {name}. Status code: {response.status_code}")
+            failed_docs.append(name)
     except Exception as e:  
         print(f"Error downloading {name}: {type(e).__name__ } : {e}")
+        failed_docs.append(name)
+
+unique_doc = unique_doc[~unique_doc['doc_name'].isin(failed_docs)]
 
 #Create a dataset of a subset of the documents to use for testing.
+
+test_df = unique_doc[:10]
+print(test_df['doc_name'][:])
+
+#Read pdfs and convert them to text using docling
+source = "C:\\Users\\Tommy\\RagEval\\test_pdfs\\3M_2018_10K.pdf"
+converter = DocumentConverter()
+res = converter.convert(source)
+
+#Chunk the text into smaller pieces using langchain_text_splitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+chunks = text_splitter.split_text(res['text'])
+
+
 
 
 
